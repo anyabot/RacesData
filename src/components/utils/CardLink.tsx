781ba8f6386abfb4a4ecaa4f6b3d436b849d3626
anyabot <52899201+anyabot@users.noreply.github.com>
2023-06-10 11:24:17 +0900

    card animattion

--- src/components/utils/CardLink.tsx
+++ src/components/utils/CardLink.tsx
@@ -16,11 +16,11 @@ interface Props {
 
 export default function CardLink({text, img, href}: Props) {
   return (
-    <Link href={href} className="inline-block">
-    <Card className="mt-6">
+    <Link href={href} className="inline-block hover:scale-105 hover:transition-all hover:shadow-lg">
+    <Card className="mt-6 hover:text-blue-500">
       <CardBody>
       <img src={img} alt="img-blur-shadow"/>
-        <Typography variant="h5" color="blue-gray" className="mt-4 min-h-96">
+        <Typography variant="h5" color="blue-gray" className="mt-4 min-h-96 text-center text-inherit">
           {text}
         </Typography>
         
